b144735eb3d5c1a01b9f41f0a250864c03e4c8dd
somyungsub <gkdldy5@naver.com>
2018-05-24 16:27:05 +0900

    New : AngularJS 01.Day

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice1'},
+  {id: 12, name: 'Mr. Nice2'},
+  {id: 13, name: 'Mr. Nice3'},
+  {id: 14, name: 'Mr. Nice4'},
+  {id: 15, name: 'Mr. Nice5'},
+  {id: 16, name: 'Mr. Nice6'},
+  {id: 17, name: 'Mr. Nice7'},
+  {id: 18, name: 'Mr. Nice8'},
+  {id: 19, name: 'Mr. Nice9'},
+  {id: 20, name: 'Mr. Nice10'}
+]
+;
